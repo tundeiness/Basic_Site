162d6433d62034cf55be6e12581c89be7fd9ea75
tundeiness <tundeiness@yahoo.co.uk>
2020-08-30 13:57:44 +0100

    create url parser

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
 
+  const qweree = url.parse(req.url, true);
+  const filename = "." + qweree.pathname;
+
+  const fsCallback =
+
+
 })
